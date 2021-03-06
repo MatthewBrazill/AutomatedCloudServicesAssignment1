81f760e6814612fa5ad25274f02ea38c6ad12bd5
Matthew Brazill <Matthew@Brazill.de>
2021-03-06 17:03:13 +0000

    Instance creation fully finctional

--- newwebserver.py
+++ newwebserver.py
@@ -7,12 +7,15 @@ import sys
 import boto3
 import logging
 import asyncio
+import os
 
 # Resources
 s3 = boto3.resource("s3")
 ec2 = boto3.resource("ec2")
 
 # Logging Configuration
+if not os.path.exists("./logs"):
+    os.makedirs("./logs")
 logging.basicConfig(
     format="%(asctime)s %(levelname)s: %(message)s",
     filename="./logs/logfile.log",
@@ -38,9 +41,9 @@ logging.info("Finding aws account ID: %s", accountID)
 async def main():
     """The main function to start all the processes and gather user input.
 
-    After gathering the details from the user the program starts all the 
-    individual task off, such as the creation of the S3 Bucket, the creation 
-    of the EC2 instance and the launching of the Apache server. If the user 
+    After gathering the details from the user the program starts all the
+    individual task off, such as the creation of the S3 Bucket, the creation
+    of the EC2 instance and the launching of the Apache server. If the user
     doesn't provide an input a generic one is generated.
     """
 
@@ -49,7 +52,7 @@ async def main():
     if "--bucket_name" in sys.argv:
         bucketName = sys.argv[sys.argv.index("-bucket_name") + 1]
     else:
-        bucketName = f"webserver-bucket-{processID}"
+        bucketName = "webserver-assignment-bucket-brazill"
     logging.info("Bucket name set to '%s'.", bucketName)
 
     if "--instance_name" in sys.argv:
@@ -58,32 +61,25 @@ async def main():
         instanceName = f"webserver-{processID}"
     logging.info("EC2 Instance name set to '%s'.", instanceName)
 
-    if "--key_name" in sys.argv:
-        keyName = sys.argv[sys.argv.index("-key") + 1]
-        logging.info("Using existing key...")
-    else:
-        keyName = f"webserver-key-{processID}"
-
     if "--web_files_path" in sys.argv:
         webFilesPath = sys.argv[sys.argv.index("-web_files_path") + 1]
-        logging.info("Loading the startup script from '%s'...", webFilesPath)
-        startupScript = open(webFilesPath, "r")
+        logging.info("Web files path set to '%s'.", webFilesPath)
     else:
-        webFilesPath = "./DefaultWebserver"
-        logging.info("Using default webfiles...")
-        startupScript = open("./DefaultWebserver/startupScript.txt", "r")
-        print(startupScript.read())
+        webFilesPath = "./webserver_files"
+        logging.info("Using default webfiles.")
+
+    keyName = f"webserver-key-{processID}"
+    startupScript = open(f"./startupScript.sh", "r")
 
     if "--help" in sys.argv:
         doHelp()
         return
 
     # Create the Bucket
-    bucketCreated = createBucket(bucketName)
+    bucketCreated = createBucket(bucketName, webFilesPath)
 
     # Create the Instance
-    instanceCreated = createInstance(
-        instanceName, keyName, startupScript.read())
+    instanceCreated = createInstance(instanceName, keyName, webFilesPath, startupScript.read())
     startupScript.close()
 
     await instanceCreated
@@ -91,10 +87,10 @@ async def main():
 
 
 # Function to create the Bucket
-async def createBucket(bucketName: str) -> bool:
+async def createBucket(bucketName: str, webFilesPath: str) -> bool:
     """This function creates the S3 Bucket.
 
-    This function creates the Bucket that contains the image files needed for 
+    This function creates the Bucket that contains the image files needed for
     the static webserver that will run on the EC2 instance.
     """
 
@@ -108,28 +104,34 @@ async def createBucket(bucketName: str) -> bool:
             }
         )
 
-    except:
-        logging.error(
-            "Bucket creation failed, due to an unexpected error. Aborting...")
-        return False
+    except Exception as err:
+        if err.response["Error"]["Code"] == "BucketAlreadyOwnedByYou":
+            logging.warning("Bucket already exists but belongs to you.")
+            bucket = s3.Bucket(bucketName)
+        else:
+            logging.error("Bucket creation failed: " + str(err))
+            return False
 
-    logging.info("Bucket creation succeeded.")
+    else:
+        logging.info("Bucket creation succeeded.")
 
-    return await fillBucket(bucket)
+    return await fillBucket(bucket, webFilesPath)
 
 
-async def fillBucket(bucket: object) -> bool:
+async def fillBucket(bucket: object, webFilesPath: str) -> bool:
     """This function fills the S3 bucket
 
-    After the bucket was created it needs to be filled with the necessary 
-    files to support the webserver. To have files be uploaded they need to be 
+    After the bucket was created it needs to be filled with the necessary
+    files to support the webserver. To have files be uploaded they need to be
     in the "bucket" subfolder in the webfiles folder.
     """
 
     # Uploading the Files
     try:
         logging.info("Uploading webserver files to S3 Bucket.")
-    
+        for file in os.scandir(webFilesPath+"/bucket"):
+            bucket.upload_file(webFilesPath+"/bucket/"+file.name, file.name, ExtraArgs={'ACL': 'public-read'})
+
     except Exception as err:
         logging.error("Uploading failed: " + str(err))
         return False
@@ -139,7 +141,7 @@ async def fillBucket(bucket: object) -> bool:
 
 
 # Function to create the instance
-async def createInstance(instanceName: str, keyName: str, startupScript: str) -> bool:
+async def createInstance(instanceName: str, keyName: str, webFilesPath: str, startupScript: str) -> bool:
     """This function creates the EC2 Instance
 
     To host the Webserver the EC2 instance is created to run everything.
@@ -161,6 +163,8 @@ async def createInstance(instanceName: str, keyName: str, startupScript: str) ->
                 },
             ]
         )
+        keyFile = open(keyPair.key_name + ".pem", "w")
+        keyFile.write(keyPair.key_material)
 
     except Exception as err:
         logging.error("Key Pair creation failed: " + str(err))
@@ -226,36 +230,21 @@ async def createInstance(instanceName: str, keyName: str, startupScript: str) ->
         logging.info("Security Group creation succeeded.")
 
     try:
-        print(startupScript, keyPair)
         # Creating the EC2 Instance
         instance = ec2.create_instances(
-            BlockDeviceMappings=[
-                {
-                    "Ebs": {
-                        "DeleteOnTermination": True,
-                        "VolumeSize": 16,
-                        "VolumeType": "standard"
-                    }
-                },
-            ],
             ImageId="ami-096f43ef67d75e998",
             InstanceType="t2.nano",
-            KeyName=str(await keyPair.key_name),
+            KeyName=keyPair.key_name,
             MaxCount=1,
             MinCount=1,
             Monitoring={
                 "Enabled": False
             },
-            SecurityGroups=[str(await securityGroup.group_name)],
+            SecurityGroups=[securityGroup.group_name],
             UserData=startupScript,
             DisableApiTermination=False,
             EbsOptimized=False,
-            IamInstanceProfile={
-                "Arn": "string",
-                "Name": "string"
-            },
             InstanceInitiatedShutdownBehavior="terminate",
-            PrivateIpAddress="10.0.0.100",
             TagSpecifications=[
                 {
                     "ResourceType": "instance",
@@ -268,6 +257,8 @@ async def createInstance(instanceName: str, keyName: str, startupScript: str) ->
                 }
             ]
         )[0]
+        instance.wait_until_running()
+        instance.reload()
 
     except Exception as err:
         logging.error("Instance creation failed: " + str(err))
@@ -286,4 +277,4 @@ def doHelp():
 # Starting the program
 logging.info("Starting program...")
 asyncio.get_event_loop().run_until_complete(main())
-logging.info("Program complete.\n\n\n")
+logging.info("Program complete. The web server should now be active.\n\n\n")
